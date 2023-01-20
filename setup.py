a1f67c64205fffcbdaae4642a7c0d352b36295d4
pop-srw <khemmachotikun.s@gmail.com>
2023-01-21 01:36:19 +0700

    bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="streamlit-cognito-auth",
-    version="0.1.5",
+    version="1.0.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
