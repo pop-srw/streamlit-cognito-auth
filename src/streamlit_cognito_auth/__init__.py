3bce1a607d678dfc8785b38ce72fec8435c764eb
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2023-07-14 17:10:23 +0200

    Easier imports

--- src/streamlit_cognito_auth/__init__.py
+++ src/streamlit_cognito_auth/__init__.py
@@ -1,1 +1,4 @@
 from .auth import CognitoAuthenticator, CognitoHostedUIAuthenticator
+from .session_provider import Boto3SessionProvider
+
+__all__ = ["CognitoAuthenticator", "CognitoHostedUIAuthenticator", "Boto3SessionProvider"]
