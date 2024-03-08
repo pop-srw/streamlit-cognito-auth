912d23919d4af478d2def7289287a27128e8dfb2
Corentin <secured.alt@tutanota.com>
2024-03-08 12:35:04 +0100

    Patch 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="streamlit-cognito-auth",
-    version="1.2.0",
+    version="1.2.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
